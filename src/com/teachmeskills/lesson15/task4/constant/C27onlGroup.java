980bd4cce027691172ccd7e96fc21a4e795126c7
IvanFLINT <rpkdevil@gmail.com>
2024-02-26 07:22:32 +0300

    Lesson_15

---
+++ src/com/teachmeskills/lesson15/task4/constant/C27onlGroup.java
@@ -0,0 +1,26 @@
+package com.teachmeskills.lesson15.task4.constant;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface C27onlGroup {
+    List<String> GROUP = Arrays.asList(
+            "Артём",
+            "Алина",
+            "Анастасия",
+            "Артём",
+            "Никита",
+            "Олег",
+            "Иван",
+            "Кирилл",
+            "Руслан",
+            "Евгений",
+            "Павел",
+            "Руслан",
+            "Руслан",
+            "Александр",
+            "Даниил",
+            "Алексей",
+            "Владислав",
+            "Андрей");
+}
---
+++ src/com/teachmeskills/lesson15/task4/service/Service.java
@@ -0,0 +1,9 @@
+package com.teachmeskills.lesson15.task4.service;
+
+import java.util.*;
+
+public class Service {
+    public static Set<String> service(List<String> list){
+        return new HashSet<>(list);
+    }
+}
